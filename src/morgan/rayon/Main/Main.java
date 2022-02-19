48cdd64199643ac1138a67ccb550a3255c78ea0f
Rayon Morgan <rayonaray@hotmail.com>
2022-02-19 10:59:32 -0500

    Initial Commit

---
+++ src/morgan/rayon/Main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package morgan.rayon.Main;
+
+/**
+ * @author Rayon Morgan
+ *  
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 
+
+	}
+
+}
---
+++ src/morgan/rayon/standard/TwoFactorAuthentication.java
@@ -0,0 +1,13 @@
+package morgan.rayon.standard;
+
+public class TwoFactorAuthentication {
+	
+	//Username to verify
+	String username; 
+	//password to verify
+	String password;
+	//Verification code sent to user as added security
+	String vercode;
+	
+
+}
