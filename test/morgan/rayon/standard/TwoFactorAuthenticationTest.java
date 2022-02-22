a62523dd4867ff141c999dd86b31d4dff46e540b
Rayon Morgan <rayonaray@hotmail.com>
2022-02-21 22:13:55 -0500

    Update to Unit test: generateCode method tested

--- test/morgan/rayon/standard/TwoFactorAuthenticationTest.java
+++ test/morgan/rayon/standard/TwoFactorAuthenticationTest.java
@@ -2,13 +2,25 @@ package morgan.rayon.standard;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TwoFactorAuthenticationTest {
+	
+	TwoFactorAuthentication twofactorauthentication; 
+	
+	@Before 
+	public void ExecuteBefore()
+	{
+		twofactorauthentication = new TwoFactorAuthentication(); 
+	}
 
 	@Test
 	public void testGenerateCode_sizeFour() {
-		fail("Not yet implemented");
+		
+		 int test_size = 4;
+		 assertEquals(test_size, twofactorauthentication.generateCode(test_size).length() );
 	}
 
 }
