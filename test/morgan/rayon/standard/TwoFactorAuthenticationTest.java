0aede27997a4e59ec5d509e91e627488d2007158
Rayon Morgan <rayonaray@hotmail.com>
2022-02-22 22:43:07 -0500

    addition of JUnit testin class:TwoFactorAuthentication

--- test/morgan/rayon/standard/TwoFactorAuthenticationTest.java
+++ test/morgan/rayon/standard/TwoFactorAuthenticationTest.java
@@ -3,7 +3,6 @@ package morgan.rayon.standard;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TwoFactorAuthenticationTest {
@@ -21,6 +20,16 @@ public class TwoFactorAuthenticationTest {
 		
 		 int test_size = 4;
 		 assertEquals(test_size, twofactorauthentication.generateCode(test_size).length() );
+	}
+	
+	@Test
+	public void testGenerateCode_alphanumeric_sizeFour()
+	{
+		int test_size = 4;
+		String result = twofactorauthentication.generateCode(test_size);
+		boolean isvalid = result.matches("^[^A-Za-z0-9]+$");		 
+		assertFalse(isvalid);		
+		
 	}
 
 }
